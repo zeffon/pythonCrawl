c8f15f8ea8de5e37426a132e7108fa405cdd7007
zeffon <zephoony#gmail.com>
2017-04-23 16:49:24 +0800

    books_crawler init done

---
+++ books_crawler/books_crawler/spiders/books.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class BooksSpider(CrawlSpider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ['http://books.toscrape.com/']
+    rules = (Rule(LinkExtractor(), callback='parse_page', follow=False),)
+    def parse_page(self, response):
+        yield {'URL': response.url}
