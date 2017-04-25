b6e9e2fb655981118b602ca266f62e261a9725ab
zeffon <zephoony#gmail.com>
2017-04-26 07:14:48 +0800

    get the books list info

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -10,39 +10,60 @@
 #     rules = (Rule(LinkExtractor(), callback='parse_page', follow=False),)
 #     def parse_page(self, response):
 #         yield {'URL': response.url}
+
+
+# import scrapy
+# from scrapy.spiders import Spider
+# from scrapy.selector import Selector
+# from selenium import webdriver
+# from scrapy.http import Request
+# from selenium.common.exceptions import NoSuchElementException
+# from time import sleep
+#
+# class BooksSpider(Spider):
+#     name = 'books'
+#     allowed_domains = ['books.toscrape.com']
+#     def start_requests(self):
+#         self.driver = webdriver.Chrome('/Users/yuzefeng/Documents/Tools/python/chromedriver')
+#         self.driver.get('http://books.toscrape.com')
+#         sel = Selector(text=self.driver.page_source)
+#         books = sel.xpath('//h3/a/@href').extract()
+#         for book in books:
+#             url = 'http://books.toscrape.com/' + book
+#             yield Request(url, callback=self.parse_book)
+#         while True:
+#             try:
+#                 element = self.driver.find_element_by_xpath('//a[text()="next"]')
+#                 sleep(3)
+#                 self.logger.info('sleep 3 seconds')
+#                 element.click()
+#                 sel = Selector(text=self.driver.page_source)
+#                 books = sel.xpath('//h3/a/@href').extract()
+#                 for book in books:
+#                     url = 'http://books.toscrape.com/catalogue/' + book
+#                     yield Request(url, callback=self.parse_book)
+#             except NoSuchElementException:
+#                 self.logger.info('No Such ElementException')
+#                 self.driver.quit()
+#                 break
+#     def parse_book(self, response):
+#         pass
 import scrapy
 from scrapy.spiders import Spider
-from scrapy.selector import Selector
-from selenium import webdriver
 from scrapy.http import Request
-from selenium.common.exceptions import NoSuchElementException
-from time import sleep
+
 
 class BooksSpider(Spider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
-    def start_requests(self):
-        self.driver = webdriver.Chrome('/Users/yuzefeng/Documents/Tools/python/chromedriver')
-        self.driver.get('http://books.toscrape.com')
-        sel = Selector(text=self.driver.page_source)
-        books = sel.xpath('//h3/a/@href').extract()
+    start_urls = ['http://books.toscrape.com']
+    def parse(self, response):
+        books = response.xpath('//h3/a/@href').extract()
         for book in books:
-            url = 'http://books.toscrape.com/' + book
-            yield Request(url, callback=self.parse_book)
-        while True:
-            try:
-                element = self.driver.find_element_by_xpath('//a[text()="next"]')
-                sleep(3)
-                self.logger.info('sleep 3 seconds')
-                element.click()
-                sel = Selector(text=self.driver.page_source)
-                books = sel.xpath('//h3/a/@href').extract()
-                for book in books:
-                    url = 'http://books.toscrape.com/catalogue/' + book
-                    yield Request(url, callback=self.parse_book)
-            except NoSuchElementException:
-                self.logger.info('No Such ElementException')
-                self.driver.quit()
-                break
+            absolute_url = response.urljoin(book)
+            yield Request(absolute_url, callback=self.parse_book)
+        next_page_url = response.xpath('//a[text()="next"]/@href').extract()[0]
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield Request(absolute_next_page_url)
     def parse_book(self, response):
         pass
